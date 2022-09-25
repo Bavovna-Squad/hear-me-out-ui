6bedb96362889b1669855418b6c8b6fb579f68b4
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-09-25 15:24:13 +0300

    [refactoring] Landing page
    Moved landing page to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { Card, Layout } from 'antd';
+import React, { FunctionComponent } from 'react';
+import { Layout } from 'antd';
 import { Button } from 'antd';
 import { Typography } from 'antd';
-import { CaretRightFilled, SmileTwoTone } from '@ant-design/icons';
-import React, { FunctionComponent } from 'react';
-import './App.css';
+import { LandingPage } from './pages/LandingPage';
 
 const { Header, Content } = Layout;
-const { Title } = Typography;
 
 export const App: FunctionComponent = () => {
     return (
@@ -19,79 +17,7 @@ export const App: FunctionComponent = () => {
                 </Button>
             </Header>
             <Content>
-                <section
-                    style={{
-                        margin: '0 auto',
-                        maxWidth: '800px',
-                        textAlign: 'center',
-                        padding: '5em 1em 1em 1em'
-                    }}
-                >
-                    <Title>
-                        Don't know what to do? <br />
-                        Try to <span className="sexy-style">guess a song</span>
-                    </Title>
-                    <Title level={5}>
-                        Hear Me Out is a mini-game, that brings fun and cool experience to you,
-                        especially when you want to guess some songs
-                    </Title>
-                    <Button
-                        size="large"
-                        shape="circle"
-                        type="primary"
-                        icon={<CaretRightFilled style={{ fontSize: '35px' }} />}
-                        style={{
-                            width: '80px',
-                            height: '80px',
-                            margin: '2em'
-                        }}
-                    >
-                    </Button>
-                </section>
-                <section
-                    style={{
-                        margin: '0 auto',
-                        maxWidth: '800px',
-                        textAlign: 'center',
-                        padding: '0em 1em 1em 1em'
-                    }}
-                >
-                    <Title level={2}>
-                        Just look what our game will bring to <span className="sexy-style">you!</span>
-                    </Title>
-                    <div className="landing-grid">
-                        <Card
-                            title={
-                                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '0.5em' }}>
-                                    <SmileTwoTone style={{ fontSize: '2em' }} />
-                                    <Typography>Fun</Typography>
-                                </div>
-                            }
-                        >
-                            <Typography>All sadness will go away. Don't believe us? Try yourself!</Typography>
-                        </Card>
-                        <Card
-                            title={
-                                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '0.5em' }}>
-                                    <SmileTwoTone style={{ fontSize: '2em' }} />
-                                    <Typography>Fun</Typography>
-                                </div>
-                            }
-                        >
-                            <Typography>All sadness will go away. Don't believe us? Try yourself!</Typography>
-                        </Card>
-                        <Card
-                            title={
-                                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '0.5em' }}>
-                                    <SmileTwoTone style={{ fontSize: '2em' }} />
-                                    <Typography>Fun</Typography>
-                                </div>
-                            }
-                        >
-                            <Typography>All sadness will go away. Don't believe us? Try yourself!</Typography>
-                        </Card>
-                    </div>
-                </section>
+                <LandingPage />
             </Content>
         </Layout>
     );
---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,113 @@
+import React, { FunctionComponent } from 'react';
+import './LandingPage.css';
+import { Button, Card, Typography } from 'antd';
+import { CaretRightFilled, SmileTwoTone } from '@ant-design/icons';
+
+const { Title } = Typography;
+
+export const LandingPage: FunctionComponent = () => {
+    return (
+        <>
+            <section
+                style={{
+                    margin: '0 auto',
+                    maxWidth: '800px',
+                    textAlign: 'center',
+                    padding: '5em 1em 1em 1em'
+                }}
+            >
+                <Title>
+                    Don't know what to do? <br />
+                    Try to <span className="sexy-style">guess a song</span>
+                </Title>
+                <Title level={5}>
+                    Hear Me Out is a mini-game, that brings fun and cool experience to you,
+                    especially when you want to guess some songs
+                </Title>
+                <Button
+                    size="large"
+                    shape="circle"
+                    type="primary"
+                    icon={<CaretRightFilled style={{ fontSize: '35px' }} />}
+                    style={{
+                        width: '80px',
+                        height: '80px',
+                        margin: '2em'
+                    }}
+                >
+                </Button>
+            </section>
+            <section
+                style={{
+                    margin: '0 auto',
+                    maxWidth: '800px',
+                    textAlign: 'center',
+                    padding: '0em 1em 1em 1em'
+                }}
+            >
+                <Title level={2}>
+                    Just look what our game will bring to <span className="sexy-style">you!</span>
+                </Title>
+                <div className="landing-grid">
+                    <Card
+                        title={
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    gap: '0.5em'
+                                }}
+                            >
+                                <SmileTwoTone style={{ fontSize: '2em' }} />
+                                <Typography>Fun</Typography>
+                            </div>
+                        }
+                    >
+                        <Typography>
+                            All sadness will go away. Don't believe us? Try yourself!
+                        </Typography>
+                    </Card>
+                    <Card
+                        title={
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    gap: '0.5em'
+                                }}
+                            >
+                                <SmileTwoTone style={{ fontSize: '2em' }} />
+                                <Typography>Fun</Typography>
+                            </div>
+                        }
+                    >
+                        <Typography>
+                            All sadness will go away. Don't believe us? Try yourself!
+                        </Typography>
+                    </Card>
+                    <Card
+                        title={
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    gap: '0.5em'
+                                }}
+                            >
+                                <SmileTwoTone style={{ fontSize: '2em' }} />
+                                <Typography>Fun</Typography>
+                            </div>
+                        }
+                    >
+                        <Typography>
+                            All sadness will go away. Don't believe us? Try yourself!
+                        </Typography>
+                    </Card>
+                </div>
+            </section>
+        </>
+    );
+};
