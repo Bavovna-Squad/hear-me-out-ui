2e0afac7ebeaf99a0ccf8e17f15cda5adc2eb6cf
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-09-25 15:28:47 +0300

    [maintenance] Added recoil state manager

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import React, { FunctionComponent } from 'react';
 import { Layout } from 'antd';
 import { Button } from 'antd';
 import { Typography } from 'antd';
+import { RecoilRoot } from 'recoil';
 import { LandingPage } from './pages/LandingPage';
 
 const { Header, Content } = Layout;
 
 export const App: FunctionComponent = () => {
     return (
-        <Layout>
-            <Header style={{ textAlign: 'center' }}>
-                <Button type="text" size="large" style={{ textTransform: 'uppercase' }}>
-                    <Typography style={{ fontWeight: 'bold' }}>
-                        Hear Me <span className="sexy-style">Out</span>
-                    </Typography>
-                </Button>
-            </Header>
-            <Content>
-                <LandingPage />
-            </Content>
-        </Layout>
+        <RecoilRoot>
+            <Layout>
+                <Header style={{ textAlign: 'center' }}>
+                    <Button type="text" size="large" style={{ textTransform: 'uppercase' }}>
+                        <Typography style={{ fontWeight: 'bold' }}>
+                            Hear Me <span className="sexy-style">Out</span>
+                        </Typography>
+                    </Button>
+                </Header>
+                <Content>
+                    <LandingPage />
+                </Content>
+            </Layout>
+        </RecoilRoot>
     );
 };
