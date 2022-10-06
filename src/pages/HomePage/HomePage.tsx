4c40b4e5b965d691b7c899c1782f0799e80ac0a4
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-10-06 15:32:26 +0300

    [feature] Home page design
    Added base design of home page in empty state

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,31 @@
 import React, { FunctionComponent } from 'react';
+import { Typography, Empty, Button, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import { MainLayout } from '../../layouts/MainLayout';
+
+const { Title } = Typography;
 
 export const HomePage: FunctionComponent = () => {
     return (
         <MainLayout>
-            <section>Hello</section>
+            <section className="container flex flex-col gap-8 pt-12 pb-4 mx-auto">
+                <Title level={3} className="text-center">
+                    Hello, <span className="hmo-accented-text">FirstName LastName!</span> Ready to
+                    guess?
+                </Title>
+                <Empty
+                    description={<span>You don't have any saved challenges yet.</span>}
+                >
+                    <Tooltip title="search">
+                        <Button
+                            shape="circle"
+                            type="dashed"
+                            icon={<SearchOutlined />}
+                            size="large"
+                        />
+                    </Tooltip>
+                </Empty>
+            </section>
         </MainLayout>
     );
 };
